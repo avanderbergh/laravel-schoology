c846a245adb606e8d1c81fed50be3f203eb17b3d
Adriaan van der Bergh <avanderbergh@gmail.com>
2015-07-01 16:39:29 +0200

    Add Facade

---
+++ src/Avanderbergh/Schoology/Facades/Saml2Auth.php
@@ -0,0 +1,19 @@
+<?php
+namespace Avanderbergh\Schoology\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Saml2Auth extends Facade
+{
+
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'Avanderbergh\Schoology\Saml2Auth';
+    }
+
+} 
