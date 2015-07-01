c59b3ee3044b1012b004ffc11a04adbf628c5e14
Adriaan van der Bergh <avanderbergh@gmail.com>
2015-07-01 17:04:55 +0200

    Fix migrations

--- src/migrations/create_oauth_stores_table.php
+++ src/migrations/create_oauth_stores_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateOauthStoreTable extends Migration {
+class CreateOauthStoresTable extends Migration {
 
 	/**
 	 * Run the migrations.
